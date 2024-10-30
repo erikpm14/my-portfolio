c0399f11884eb6c0a44532326f9ba55d27a169cb
erikpm14 <portilloerik14@gmail.com>
2024-10-30 19:57:15 +0100

    mejora

--- my-portafolio/pages/home/home.js
+++ my-portafolio/pages/home/home.js
@@ -7,8 +7,8 @@ export const home = () => {
   main.innerHTML = `
     <section class="home">
     <p>Hey, I'm</p>
-    <h1>Peter Parker</h1>
-    <p>I'm a software engineer based in Toronto, Canada and also a communication and journalism student. I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I have been freelancing for a year now while studying at the university and I've manage to gain a decent amount of experience and valuable knowledge from all different kinds of fields throughout my projects/work.</p>
+    <h1>Erik Portillo</h1>
+    <p>I'm Erik Portillo, a software engineer and a student in a higher-level Web Application Development (DAW) program, which I've been studying for two months. I live in Oropesa del Mar and balance my studies with work. I'm passionate about creating digital projects that live on the internet, whether they're websites, applications, or anything in between. I've been freelancing for a year while studying, which has allowed me to gain valuable experience across various fields through my projects and work.</p>
     <a href="mailto:peterparker@gmail.com">Say hi →</a>
     </section> `;
 };
